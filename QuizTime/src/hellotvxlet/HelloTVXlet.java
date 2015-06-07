7d9e7a6cc3acd70952082b0984620ffff3bdea57
cornelisjw <jan.willem@pandora.be>
2015-06-07 16:54:47 +0200

    Array Update

--- QuizTime/src/hellotvxlet/HelloTVXlet.java
+++ QuizTime/src/hellotvxlet/HelloTVXlet.java
@@ -26,8 +26,18 @@ public class HelloTVXlet implements Xlet, HActionListener, ResourceClient
     private HStaticText lblTotalPoints;
 
     private int intPoints = 0;
-    private int i = 0;
+    private int i = -1; //First right = i++ 
     
+   private String[][] arrayQuestion = { 
+        {"How many planets does the solar system count?", "8", "9","7","6","Right","Wrong","Wrong","Wrong"},
+        {"Wich planet is the hottest?", "Earth", "Mercury","Venus","Jupiter","Wrong","Wrong","Right","Wrong"},
+        {"Wich planet is the biggest?", "Mars", "Saturn","Uranus","Jupiter","Wrong","Wrong","Wrong","Right"},
+        {"Wich planet is the farest from the sun?", "Jupiter", "Earth","Pluto","Saturn","Wrong","Wrong","Right","Wrong"},
+        {"Wich is the brightest object in the sky?", "Venus", "Mars","Mercury","Pluto","Right","Wrong","Wrong","Wrong"},
+        {"Who was the first astronaut to walk on the moon?", "Buzz Aldrin", "Andrei Borisenko","Dirk Frimout","Neill Armstrong","Wrong","Wrong","Wrong","Right"},
+        {"What country had the first space station?", "America", "Russia","China","Europe","Wrong","Right","Wrong","Wrong"},
+        {"Which was the first planet seen by a telescope?", "Mars", "Uranus","Mercury","Jupiter","Wrong","Right","Wrong","Wrong"},
+    };
     public void notifyRelease (ResourceProxy proxy)
     { 
     
@@ -71,34 +81,34 @@ public class HelloTVXlet implements Xlet, HActionListener, ResourceClient
        *  QUESTION SCHERM
        * 
       */
-      lblQuestion = new HStaticText("How many planets does the solar system count?");
+      lblQuestion = new HStaticText();
       lblQuestion.setLocation(100, 40);
       lblQuestion.setSize(500,50);
       lblQuestion.setBackground(new DVBColor(0, 0, 0, 179));
       lblQuestion.setVisible(false);
       
-      btnAnswerA = new HTextButton("8");
+      btnAnswerA = new HTextButton();
       btnAnswerA.setLocation(245,150);
       btnAnswerA.setSize(250,50);
       btnAnswerA.setBackground(new DVBColor(0, 0, 0, 179));
       btnAnswerA.setBackgroundMode(HVisible.BACKGROUND_FILL);
       btnAnswerA.setVisible(false);
 
-      btnAnswerB = new HTextButton("9");
+      btnAnswerB = new HTextButton();
       btnAnswerB.setLocation(245,220);
       btnAnswerB.setSize(250,50);
       btnAnswerB.setBackground(new DVBColor(0, 0, 0, 179));
       btnAnswerB.setBackgroundMode(HVisible.BACKGROUND_FILL);
       btnAnswerB.setVisible(false);
             
-      btnAnswerC = new HTextButton("7");
+      btnAnswerC = new HTextButton();
       btnAnswerC.setLocation(245,290);
       btnAnswerC.setSize(250,50);
       btnAnswerC.setBackground(new DVBColor(0, 0, 0, 179));
       btnAnswerC.setBackgroundMode(HVisible.BACKGROUND_FILL);
       btnAnswerC.setVisible(false);
       
-      btnAnswerD = new HTextButton("6");
+      btnAnswerD = new HTextButton();
       btnAnswerD.setLocation(245,360);
       btnAnswerD.setSize(250,50);
       btnAnswerD.setBackground(new DVBColor(0, 0, 0, 179));
@@ -162,16 +172,11 @@ public class HelloTVXlet implements Xlet, HActionListener, ResourceClient
       btnStart.requestFocus();
       
       /**
-       *  FIRST QUESTION AWNSERS + LISENERS
+       *  LISENERS
        * 
       */      
-      btnAnswerA.setActionCommand("Right");
-      btnAnswerB.setActionCommand("Wrong");
-      btnAnswerC.setActionCommand("Wrong");
-      btnAnswerD.setActionCommand("Wrong");
+      btnStart.setActionCommand("Right");
       btnClose.setActionCommand("stop");
-      btnStart.setActionCommand("Intro");
-      
       btnAnswerA.addHActionListener(this);
       btnAnswerC.addHActionListener(this);
       btnAnswerB.addHActionListener(this);
@@ -193,7 +198,50 @@ public class HelloTVXlet implements Xlet, HActionListener, ResourceClient
     public void destroyXlet(boolean unconditional) throws XletStateChangeException
     {
     }
-    
+    public void endGame(){
+           scene.setBackground( new DVBColor(0, 0, 0, 200) );
+           scene.setBackgroundMode( HVisible.BACKGROUND_FILL ); 
+           btnAnswerA.setVisible(false);
+           btnAnswerB.setVisible(false);
+           btnAnswerC.setVisible(false);
+           btnAnswerD.setVisible(true);
+           btnClose.setVisible(false);
+           lblQuestion.setVisible(false);
+           lblTotalPoints.setVisible(true);
+           lblPoints.setLocation(320,230);
+           intPoints = -1;
+           i = 0;
+           btnAnswerD.setTextContent("Retry", HState.NORMAL_STATE);
+           btnAnswerD.setActionCommand("Right");
+     }
+    public void createQuestion(int i){
+            
+            btnStart.setVisible(false);
+            lblIntro.setVisible(false);
+            btnAnswerA.setVisible(true);
+            btnAnswerB.setVisible(true);
+            btnAnswerC.setVisible(true);
+            btnAnswerD.setVisible(true);
+            btnClose.setVisible(true);
+            lblPoints.setVisible(true);
+            lblQuestion.setVisible(true);
+            scene.setVisible(true);
+            lblPoints.setLocation(600, 480);
+            lblTotalPoints.setVisible(false);
+
+            btnAnswerA.requestFocus();
+                    
+            lblQuestion.setTextContent(arrayQuestion[i][0], HState.NORMAL_STATE);
+            btnAnswerA.setTextContent(arrayQuestion[i][1], HState.NORMAL_STATE);
+            btnAnswerB.setTextContent(arrayQuestion[i][2], HState.NORMAL_STATE);
+            btnAnswerC.setTextContent(arrayQuestion[i][3], HState.NORMAL_STATE);
+            btnAnswerD.setTextContent(arrayQuestion[i][4], HState.NORMAL_STATE);
+                   
+            btnAnswerA.setActionCommand(arrayQuestion[i][5]);
+            btnAnswerB.setActionCommand(arrayQuestion[i][6]);
+            btnAnswerC.setActionCommand(arrayQuestion[i][7]);
+            btnAnswerD.setActionCommand(arrayQuestion[i][8]);
+    }
     public void actionPerformed(ActionEvent e)
     {
       /**
@@ -218,183 +266,17 @@ public class HelloTVXlet implements Xlet, HActionListener, ResourceClient
             System.out.println(intPoints);
             i++;
             
-            switch (i)
-            {
-                case 1:
-                    lblQuestion.setTextContent("What is the distance between The Earth and The Moon (Lunar Distance)?", HState.NORMAL_STATE);
-                    btnAnswerA.setTextContent("3844000km", HState.NORMAL_STATE);
-                    btnAnswerB.setTextContent("384400km", HState.NORMAL_STATE);
-                    btnAnswerC.setTextContent("43880", HState.NORMAL_STATE);
-                    btnAnswerD.setTextContent("438800", HState.NORMAL_STATE);
-                   
-                    btnAnswerA.setActionCommand("Wrong");
-                    btnAnswerB.setActionCommand("Right");
-                    btnAnswerC.setActionCommand("Wrong");
-                    btnAnswerD.setActionCommand("Wrong");
-                    break;
-                
-                case 2:
+            if(i == 8){                  
+                endGame();
+            }else{createQuestion(i);}
+            
+            
 
-                    lblQuestion.setTextContent("Wich planet is the hottest?", HState.NORMAL_STATE);
-                    btnAnswerA.setTextContent("Earth", HState.NORMAL_STATE);
-                    btnAnswerB.setTextContent("Mercury", HState.NORMAL_STATE);
-                    btnAnswerC.setTextContent("Venus", HState.NORMAL_STATE);
-                    btnAnswerD.setTextContent("Jupiter", HState.NORMAL_STATE);
-                    
-                    btnAnswerA.setActionCommand("Wrong");
-                    btnAnswerB.setActionCommand("Wrong");
-                    btnAnswerC.setActionCommand("Right");
-                    btnAnswerD.setActionCommand("Wrong");
-                    break;
-                    
-                case 3:
-
-                    lblQuestion.setTextContent("Wich planet is the biggest?", HState.NORMAL_STATE);
-                    btnAnswerA.setTextContent("Mars", HState.NORMAL_STATE);
-                    btnAnswerB.setTextContent("Saturn", HState.NORMAL_STATE);
-                    btnAnswerC.setTextContent("Uranus", HState.NORMAL_STATE);
-                    btnAnswerD.setTextContent("Jupiter", HState.NORMAL_STATE);
-                    
-                    btnAnswerA.setActionCommand("Wrong");
-                    btnAnswerB.setActionCommand("Wrong");
-                    btnAnswerC.setActionCommand("Wrong");
-                    btnAnswerD.setActionCommand("Right");
-                    break;
-                
-                case 4:
-
-                    lblQuestion.setTextContent("Wich planet is the farest from the sun?", HState.NORMAL_STATE);                    
-                    btnAnswerA.setTextContent("Jupiter", HState.NORMAL_STATE);
-                    btnAnswerB.setTextContent("Earth", HState.NORMAL_STATE);
-                    btnAnswerC.setTextContent("Pluto", HState.NORMAL_STATE);
-                    btnAnswerD.setTextContent("Saturn", HState.NORMAL_STATE);
-                    
-                    btnAnswerA.setActionCommand("Wrong");
-                    btnAnswerB.setActionCommand("Wrong");
-                    btnAnswerC.setActionCommand("Right");
-                    btnAnswerD.setActionCommand("Wrong");
-                    break;
-                
-                case 5:
-
-                    lblQuestion.setTextContent("Wich is the brightest object in the sky?", HState.NORMAL_STATE);                     
-                    btnAnswerA.setTextContent("Venus", HState.NORMAL_STATE);
-                    btnAnswerB.setTextContent("Mars", HState.NORMAL_STATE);
-                    btnAnswerC.setTextContent("Mercury", HState.NORMAL_STATE);
-                    btnAnswerD.setTextContent("plut", HState.NORMAL_STATE);
-                    
-                    btnAnswerA.setActionCommand("Right");
-                    btnAnswerB.setActionCommand("Wrong");
-                    btnAnswerC.setActionCommand("Wrong");
-                    btnAnswerD.setActionCommand("Wrong");
-                    break;
-                    
-                case 6:
-
-                    lblQuestion.setTextContent("Who was the first astronaut to walk on the moon?", HState.NORMAL_STATE);                     
-                    btnAnswerA.setTextContent("Buzz Aldrin", HState.NORMAL_STATE);
-                    btnAnswerB.setTextContent("Andrei Borisenko", HState.NORMAL_STATE);
-                    btnAnswerC.setTextContent("Dirk Frimout", HState.NORMAL_STATE);
-                    btnAnswerD.setTextContent("Neill Armstrong", HState.NORMAL_STATE);
-                    
-                    btnAnswerA.setActionCommand("Wrong");
-                    btnAnswerB.setActionCommand("Wrong");
-                    btnAnswerC.setActionCommand("Wrong");
-                    btnAnswerD.setActionCommand("Right");
-                    break;
-                    
-                case 7:
-
-                    lblQuestion.setTextContent("What year was the challenger disaster?", HState.NORMAL_STATE);                     
-                    btnAnswerA.setTextContent("1989", HState.NORMAL_STATE);
-                    btnAnswerB.setTextContent("1987", HState.NORMAL_STATE);
-                    btnAnswerC.setTextContent("1981", HState.NORMAL_STATE);
-                    btnAnswerD.setTextContent("1986", HState.NORMAL_STATE);
-                    
-                    btnAnswerA.setActionCommand("Wrong");
-                    btnAnswerB.setActionCommand("Wrong");
-                    btnAnswerC.setActionCommand("Wrong");
-                    btnAnswerD.setActionCommand("Right");
-                    break;
-                
-                case 8:
-
-                    lblQuestion.setTextContent("What country had the first space station?", HState.NORMAL_STATE);                                         
-                    btnAnswerA.setTextContent("America", HState.NORMAL_STATE);
-                    btnAnswerB.setTextContent("Russia", HState.NORMAL_STATE);
-                    btnAnswerC.setTextContent("China", HState.NORMAL_STATE);
-                    btnAnswerD.setTextContent("Europe", HState.NORMAL_STATE);
-                    
-                    btnAnswerA.setActionCommand("Wrong");
-                    btnAnswerB.setActionCommand("Right");
-                    btnAnswerC.setActionCommand("Wrong");
-                    btnAnswerD.setActionCommand("Wrong");
-                    break;
-                    
-                case 9:
-
-                    lblQuestion.setTextContent("Which was the first planet seen by a telescope?", HState.NORMAL_STATE);                     
-                    btnAnswerA.setTextContent("Mars", HState.NORMAL_STATE);
-                    btnAnswerB.setTextContent("Uranus", HState.NORMAL_STATE);
-                    btnAnswerC.setTextContent("Mercury", HState.NORMAL_STATE);
-                    btnAnswerD.setTextContent("Jupiter", HState.NORMAL_STATE);
-                    
-                    btnAnswerA.setActionCommand("Wrong");
-                    btnAnswerB.setActionCommand("Right");
-                    btnAnswerC.setActionCommand("Wrong");
-                    btnAnswerD.setActionCommand("Wrong");
-                    break;
-                    
-                    
-                case 10:
-                    /**
-                     *  GAME END
-                     * 
-                     */  
-                    btnAnswerA.setVisible(false);
-                    btnAnswerB.setVisible(false);
-                    btnAnswerC.setVisible(false);
-                    btnAnswerD.setVisible(false);
-                    btnClose.setVisible(false);
-                    lblQuestion.setVisible(false);
-                    lblTotalPoints.setVisible(true);
-                    lblPoints.setLocation(320,230);
-            }
+    
         }
         else if(e.getActionCommand().equals("stop"))
         {
-           scene.setBackground( new DVBColor(0, 0, 0, 200) );
-           scene.setBackgroundMode( HVisible.BACKGROUND_FILL ); 
-           btnAnswerA.setVisible(false);
-           btnAnswerB.setVisible(false);
-           btnAnswerC.setVisible(false);
-           btnAnswerD.setVisible(true);
-           btnClose.setVisible(false);
-           lblQuestion.setVisible(false);
-           lblTotalPoints.setVisible(true);
-           lblPoints.setLocation(320,230);
-           intPoints = -1;
-           i = 0;
-           btnAnswerD.setTextContent("Retry", HState.NORMAL_STATE);
-           btnAnswerD.setActionCommand("Right");
-        }
-        else if(e.getActionCommand().equals("Intro"))
-        {
-
-            btnStart.setVisible(false);
-            lblIntro.setVisible(false);
-            btnAnswerA.setVisible(true);
-            btnAnswerB.setVisible(true);
-            btnAnswerC.setVisible(true);
-            btnAnswerD.setVisible(true);
-            btnClose.setVisible(true);
-            lblPoints.setVisible(true);
-            lblQuestion.setVisible(true);
-            scene.setVisible(true);
-           lblPoints.setLocation(600, 480);
-            lblTotalPoints.setVisible(false);
-
-            btnAnswerA.requestFocus();
+            endGame();
         }
         else if(e.getActionCommand().equals("Wrong"))
         {
@@ -402,6 +284,11 @@ public class HelloTVXlet implements Xlet, HActionListener, ResourceClient
             scene.setBackgroundMode( HVisible.BACKGROUND_FILL );
             intPoints--;
             lblPoints.setTextContent(Integer.toString(intPoints), HState.NORMAL_STATE);
+            if(intPoints == -1){
+                lblPoints.setTextContent("", HState.NORMAL_STATE);
+                lblTotalPoints.setTextContent("Game Over!", HState.NORMAL_STATE);
+                endGame();
+            }
         }
     }
 }
